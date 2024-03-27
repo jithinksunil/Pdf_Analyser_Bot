a291c40c28cfe34452a45f5a206be85e795b0ce8
jithinksunil <jithinksunil96@gmail.com>
2024-03-27 15:00:24 +0530

    updates

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,8 +9,12 @@ export class AuthService {
     return { url };
   }
   async generateGoogleTokens(code: string) {
-    const tokens = await this.commonService.generateGoogleTokens(code);
-    return { tokens, message: 'Sign in successfull' };
+    const { access_token, refresh_token } =
+      await this.commonService.generateGoogleTokens(code);
+    return {
+      tokens: { accessToken: access_token, refreshToken: refresh_token },
+      message: 'Sign in successfull',
+    };
   }
   async newAccessToken(refreshToken: string) {
     const { access_token, refresh_token } =
