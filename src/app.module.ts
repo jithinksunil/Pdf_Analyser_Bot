f4124b606b2dba7031543e0c168b5de1a132fef1
jithinksunil <jithinksunil96@gmail.com>
2024-04-08 15:31:23 +0530

    updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,20 @@ import { FileModule } from './file/file.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { AnalyserModule } from './analyser/analyser.module';
+import { CronService } from './app.cron.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     FileModule,
     AuthModule,
     AnalyserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CronService],
 })
 export class AppModule {}
