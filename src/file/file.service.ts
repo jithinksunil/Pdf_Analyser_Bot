1f6def80805adde5c37ed6c226c30d9dd2de3991
jithinksunil <jithinksunil96@gmail.com>
2024-04-10 21:18:19 +0530

    exception for files which are not pdf are added

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { GoogleService } from 'src/configurations/google-api/google.service';
 @Injectable()
 export class FileService {
   constructor(private googleService: GoogleService) {}
   async uploadFile(file: Express.Multer.File, accessToken: string) {
+    if (file.mimetype !== 'application/pdf')
+      throw new BadRequestException('Only pdf files can be uploaded');
+
     const { id: fileId, originalname: name } =
       await this.googleService.uploadFileToDrive(file, accessToken);
     return { fileId, name, message: 'Upload completed' };
